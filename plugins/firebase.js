44e90bd5b4eab3ddaa4989f21044cd74bf9fc774
Junfeng ou <junfengou@gmail.com>
2021-10-06 07:59:04 -0500

    first commit, pages are displaying correctly

---
+++ plugins/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5YGL5Mhs2HFv9nEDxexnhEisOREv1EZc",
+  authDomain: "product-review-db62b.firebaseapp.com",
+  projectId: "product-review-db62b",
+  storageBucket: "product-review-db62b.appspot.com",
+  messagingSenderId: "838729726205",
+  appId: "1:838729726205:web:6a8b706bb99857470ba694"
+};
+
+// Initialize Firebase
+// Need this additional info for nuxt
+let app = null
+if (!firebase.apps.length) {
+  app = firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase
+
